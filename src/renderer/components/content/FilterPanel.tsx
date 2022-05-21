a901791ab43a7fa19cff44a9e07aab7b4f462940
Dmitry Kashinsky <38276954+dkashinsky@users.noreply.github.com>
2022-05-21 22:57:56 +0400

    add people filter component and state management

--- src/renderer/components/content/FilterPanel.tsx
+++ src/renderer/components/content/FilterPanel.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { DesktopDatePicker } from "@mui/lab";
-import { Box, styled, TextField } from "@mui/material";
+import { Autocomplete, Box, Checkbox, styled, TextField } from "@mui/material";
+import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank';
+import CheckBoxIcon from '@mui/icons-material/CheckBox';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectEndDateFilter, selectStartDateFilter } from '../../store/files/selectors';
-import { setEndDateFilter, setStartDateFilter } from '../../store/files/actions';
+import { selectEndDateFilter, selectPeopleFilter, selectStartDateFilter } from '../../store/files/selectors';
+import { setEndDateFilter, setPeopleFilter, setStartDateFilter } from '../../store/files/actions';
+import { selectPeople } from '../../store/people/selectors';
 
 const StyledBox = styled(Box)(({ theme }) => ({
   padding: theme.spacing(1),
@@ -17,10 +20,15 @@ const StyledInput = styled(TextField)(({ theme }) => ({
   minWidth: '125px',
 }));
 
+const icon = <CheckBoxOutlineBlankIcon fontSize="small" />;
+const checkedIcon = <CheckBoxIcon fontSize="small" />;
+
 export const FilterPanel = () => {
   const dispatch = useDispatch();
   const startDate = useSelector(selectStartDateFilter);
   const endDate = useSelector(selectEndDateFilter);
+  const allPeople = useSelector(selectPeople);
+  const selectedPeople = useSelector(selectPeopleFilter);
 
   return (
     <StyledBox>
@@ -37,6 +45,30 @@ export const FilterPanel = () => {
         value={endDate}
         onChange={(date) => dispatch(setEndDateFilter(date))}
         renderInput={(params) => <StyledInput size='small' {...params} />}
+      />
+      <Autocomplete
+        size='small'
+        fullWidth
+        multiple
+        options={allPeople}
+        disableCloseOnSelect
+        getOptionLabel={(option) => option.name}
+        renderOption={(props, option, { selected }) => (
+          <li {...props}>
+            <Checkbox
+              icon={icon}
+              checkedIcon={checkedIcon}
+              style={{ marginRight: 8 }}
+              checked={selected}
+            />
+            {option.name}
+          </li>
+        )}
+        renderInput={(params) => (
+          <TextField {...params} label="People" placeholder="Select..." />
+        )}
+        value={selectedPeople}
+        onChange={(_, selection) => dispatch(setPeopleFilter(selection.map(({ id }) => id)))}
       />
     </StyledBox>
   );
